e648bdf8b810966097c292716f5ccec1ff4695f6
yuzelin <zelin.yzl@alibaba-inc.com>
2024-08-14 21:12:49 +0800

    [api][ci] Introduce paimon_python_api and ci framework

---
+++ paimon_python_api/read_builder.py
@@ -0,0 +1,43 @@
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+
+from abc import ABC, abstractmethod
+from table_read import TableRead
+from table_scan import TableScan
+from typing import List
+from typing_extensions import Self
+
+
+class ReadBuilder(ABC):
+    """An interface for building the TableScan and TableRead."""
+
+    @abstractmethod
+    def with_projection(self, projection: List[List[int]]) -> Self:
+        """Push nested projection."""
+
+    @abstractmethod
+    def with_limit(self, limit: int) -> Self:
+        """Push row number."""
+
+    @abstractmethod
+    def new_scan(self) -> TableScan:
+        """Create a TableScan to perform batch planning."""
+
+    @abstractmethod
+    def new_read(self) -> TableRead:
+        """Create a TableRead to read splits."""
